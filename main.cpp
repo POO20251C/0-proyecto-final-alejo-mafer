49451b5ba62ef4c23c98f780e7a9b08e552943f9
alejandro <boteroalejo22@gmail.com>
2025-06-05 14:36:09 -0500

    El main finalizado

--- main.cpp
+++ main.cpp
@@ -1,18 +1,41 @@
+#include <iostream>
 #include "src/Controlador.h"
-#include "string"
-#include "iostream"
 
 using namespace std;
 
 int main() {
-    //Prueba de Instanciacion de Enemigos
-    Controlador MiPrimerControlador;
-    MiPrimerControlador.InstanciarObjetos();
-    MiPrimerControlador.mostrarObjetosDisponibles();
-    MiPrimerControlador.CrearMazmorraz();
-    cout << MiPrimerControlador.Mazmorras.size() << endl;
-};
+    Controlador controlador;
 
+
+    controlador.InstanciarHeroes();
+    controlador.InstanciarEnemigos();
+    controlador.InstanciarObjetos();
+
+
+    string nombre;
+    cout << "=== Bienvenido a Natal Combat ===" << endl;
+    cout << "Ingresa tu nombre de jugador: ";
+    getline(cin, nombre);
+
+
+    Usuario* usuario = controlador.RegistrarUsuario(nombre);
+
+
+    cout << "\n=== Héroes Disponibles ===\n";
+    controlador.mostrarHeroesDisponibles();
+
+
+    cout << "\nSelecciona tus héroes (usa ↑ ↓ y ENTER para elegir, 'a' para salir):\n";
+    controlador.EscogerHeroes(usuario);
+
+
+    controlador.CrearMazmorraz();
+
+
+    controlador.Jugar(usuario);
+
+    return 0;
+}
 
 
 
