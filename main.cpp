6f874f053fa9c58ecb80d0075a6331551f0745e9
alejandro <boteroalejo22@gmail.com>
2025-06-05 14:28:03 -0500

    Main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,18 @@
-#include <string>
-#include <iostream>
+#include "src/Controlador.h"
+#include "string"
+#include "iostream"
+
+using namespace std;
+
+int main() {
+    //Prueba de Instanciacion de Enemigos
+    Controlador MiPrimerControlador;
+    MiPrimerControlador.InstanciarObjetos();
+    MiPrimerControlador.mostrarObjetosDisponibles();
+    MiPrimerControlador.CrearMazmorraz();
+    cout << MiPrimerControlador.Mazmorras.size() << endl;
+};
+
+
+
 
